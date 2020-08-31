63438c980e4236cdaad2b63aee28b09b4c118c82
saket <sk846694@gmail.com>
2020-08-31 14:34:09 +0530

    Data receiving successfully

--- .github/actions/test-action/index.js
+++ .github/actions/test-action/index.js
@@ -1,20 +1,36 @@
 const fetch = require('node-fetch');
-console.log('Hello World!')
+var fs = require('fs');
 
-fetch('https://clist.by:443/api/v1/contest/?limit=1000&end__gt=2020-08-08T03%3A07%3A43', {
-  method: 'POST',
-  headers: {
-    'content-type': 'application/json',
-    authorization: 'ApiKey Saket1999:2d2cb1a6ac690f1056748e7a71ddb7c045135d0a'
-  },
-  body: {
-    name: 'Roger',
-    age: 8
-  }
-})
-  .then(response => {
-    console.log(response)
-  })
-  .catch(err => {
-    console.log(err)
-  })
+// fetch('https://clist.by:443/api/v1/contest/?limit=1000&end__gt=2020-08-08T03%3A07%3A43', {
+//   method: 'GET',
+//   headers: {
+//     authorization: 'ApiKey Saket1999:2d2cb1a6ac690f1056748e7a71ddb7c045135d0a'
+//   }
+// })
+//   .then(response => {
+//     console.log(response.body)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
+
+async function getJSON() {
+	const response = await fetch('https://clist.by:443/api/v1/contest/?limit=1000&end__gt=2020-08-08T03%3A07%3A43', 
+		{
+			method: 'GET', 
+			headers: 
+			{
+				authorization: 'ApiKey Saket1999:2d2cb1a6ac690f1056748e7a71ddb7c045135d0a'
+			}
+		}
+		);
+	const json = await response.json();
+	console.log(json);
+	// fs.writeFile("test.json", JSON.stringify(json), function(err) {
+	//     if (err) {
+	//         console.log(err);
+	//     }
+	// });
+}
+
+getJSON();
