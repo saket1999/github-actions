d856e41abb9d34aac2fcdb1b9a972025ad6aab52
saket <sk846694@gmail.com>
2020-09-03 12:11:18 +0530

    Added modular date

--- .github/actions/test-action/index.js
+++ .github/actions/test-action/index.js
@@ -1,21 +1,11 @@
 const fetch = require('node-fetch');
 var fs = require('fs');
 
-// fetch('https://clist.by:443/api/v1/contest/?limit=1000&end__gt=2020-08-08T03%3A07%3A43', {
-//   method: 'GET',
-//   headers: {
-//     authorization: 'ApiKey Saket1999:2d2cb1a6ac690f1056748e7a71ddb7c045135d0a'
-//   }
-// })
-//   .then(response => {
-//     console.log(response.body)
-//   })
-//   .catch(err => {
-//     console.log(err)
-//   })
+var d = new Date();
+var n = d.toISOString();
 
 async function getJSON() {
-	const response = await fetch('https://clist.by:443/api/v1/contest/?limit=1000&end__gt=2020-08-08T03%3A07%3A43', 
+	const response = await fetch('https://clist.by:443/api/v1/contest/?limit=1000&end__gt=' + n, 
 		{
 			method: 'GET', 
 			headers: 
